7b8102178d286bf3c6d807a155700388be0f22dc
wojtas626 <wojtas626@gmail.com>
2019-05-25 21:46:41 +0200

    Sample mail sender in node-proxy

--- node-proxy/src/index.js
+++ node-proxy/src/index.js
@@ -2,24 +2,54 @@ const express = require('express');
 const morgan = require('morgan');
 const uuid = require('node-uuid');
 const proxy = require('express-http-proxy');
+const nodemailer = require("nodemailer");
 
-
-morgan.token('id', function getId(req) {
-    return req.id
-});
-
-const app = express();
-const port = 3000;
-app.use(assignId);
-const logPattern = '#:id (:response-time ms)|:remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"';
-app.use(morgan(logPattern));
-app.use('/proxy', proxy(`localhost:${port}`));
 
 function assignId(req, res, next) {
     req.id = uuid.v4();
     next();
 }
 
-app.get('/', (req, res) => res.send('Hello World!'));
+async function sendTestEmail() {
+    const user = 'joomla_honeypot@int.pl';
+    const pass = 'joomla:)';
+    const transporter = nodemailer.createTransport({
+        host: 'poczta.int.pl',
+        port: 465,
+        secure: true,
+        auth: {user, pass}
+    });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+    const info = await transporter.sendMail({
+        from: user,
+        to: user,
+        subject: "Hello ✔",
+        text: "Hello world?",
+        html: "<b>Hello world?</b>"
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+}
+
+
+async function main() {
+    morgan.token('id', function getId(req) {
+        return req.id
+    });
+
+    const app = express();
+    const port = 3000;
+    app.use(assignId);
+    const logPattern = '#:id (:response-time ms)|:remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"';
+    app.use(morgan(logPattern));
+    app.use('/proxy', proxy(`localhost:${port}`));
+
+    app.get('/', (req, res) => res.send('Hello World!'));
+
+    app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+    // await sendTestEmail();
+}
+
+main().catch(console.error);
