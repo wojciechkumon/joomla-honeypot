d7c492d2b30a26ba6e4bad443703ac91b53ace0c
wojtas626 <wojtas626@gmail.com>
2019-05-25 21:21:32 +0200

    morgan logging in node-proxy

--- node-proxy/src/index.js
+++ node-proxy/src/index.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const morgan = require('morgan');
+const uuid = require('node-uuid');
+
+
+morgan.token('id', function getId(req) {
+    return req.id
+});
+
 const app = express();
 const port = 3000;
+app.use(assignId);
+const logPattern = '#:id (:response-time ms)|:remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"';
+app.use(morgan(logPattern));
+
+function assignId(req, res, next) {
+    req.id = uuid.v4();
+    next();
+}
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
